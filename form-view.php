5c56c7097640c13fd3c6ed81f88e220f673a3141
Giomayra Rosales <giomayra.rosales@gmail.com>
2021-01-12 17:04:09 +0100

    update files

--- form-view.php
+++ form-view.php
@@ -1,6 +1,7 @@
 <?php // This files is mostly containing things for your view / html ?>
 <?php
 require 'index.php';
+//session_start();
 ?>
 <!doctype html>
 <html lang="en">
@@ -13,8 +14,10 @@ require 'index.php';
           rel="stylesheet"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css" type="text/css"
           rel="stylesheet"/>
-    <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+    <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'type="text/css"
+          rel="stylesheet"/>
     <link rel="stylesheet" href="styleProducts.css">
+
     <title>Your fancy store</title>
 </head>
 <body>
@@ -67,14 +70,60 @@ require 'index.php';
             </div>
         </fieldset>
 
-        <fieldset>
+<!--        <fieldset>
             <legend>Products</legend>
-            <?php foreach ($products as $i => $product): ?>
+            <?php /*foreach ($products as $i => $product): */?>
                 <label>
-                    <?php // <?p= is equal to <?php echo ?>
-                    <input type="checkbox" value="1" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
-                    &euro; <?= number_format($product['price'], 2) ?></label><br />
-            <?php endforeach; ?>
+                    <?php /*// <?p= is equal to <?php echo */?>
+                    <input type="checkbox" value="1" name="products[<?php /*echo $i */?>]"/> <?php /*echo $product['name'] */?> -
+                    &euro; <?/*= number_format($product['price'], 2) */?></label><br />
+            <?php /*endforeach; */?>
+        </fieldset>-->
+
+        <fieldset>
+           <!-- <div class="row">-->
+                <div class="col-md-4 order-md-2 mb-4">
+                    <h4 class="d-flex justify-content-between align-items-center mb-3">
+                        <span class="text-muted">Your cart</span>
+                        <span class="badge badge-secondary badge-pill">3</span>
+                    </h4>
+                    <ul class="list-group mb-3">
+                        <li class="list-group-item d-flex justify-content-between lh-condensed">
+                            <div>
+                                <h6 class="my-0">Product name</h6>
+                                <small class="text-muted">Brief description</small>
+                            </div>
+                            <span class="text-muted">$12</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between lh-condensed">
+                            <div>
+                                <h6 class="my-0">Second product</h6>
+                                <small class="text-muted">Brief description</small>
+                            </div>
+                            <span class="text-muted">$8</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between lh-condensed">
+                            <div>
+                                <h6 class="my-0">Third item</h6>
+                                <small class="text-muted">Brief description</small>
+                            </div>
+                            <span class="text-muted">$5</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between bg-light">
+                            <div class="text-success">
+                                <h6 class="my-0">Promo code</h6>
+                                <small>EXAMPLECODE</small>
+                            </div>
+                            <span class="text-success">-$5</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between">
+                            <span>Total (USD)</span>
+                            <strong>$20</strong>
+                        </li>
+                    </ul>
+                </div>
+
+           <!-- </div>-->
         </fieldset>
 
         <fieldset>
@@ -82,21 +131,39 @@ require 'index.php';
             <div class="col-md-9">
                 <div class="row g-2">
                     <?php foreach ($products as $i => $product): ?>
-                    <div class="col-md-4">
-                        <div class="product py-4">
-                            <div class="text-center"> <img src="<?php echo $product['image'] ?>" width="200"> </div>
-                            <div class="about text-center">
-                                <h5><?php echo $product['name'] ?></h5> <span> &euro; <?= number_format($product['price'], 2) ?></span>
-                            </div>
-                            <div class="cart-button mt-3 px-2 d-flex justify-content-between align-items-center"> <button class="btn btn-primary text-uppercase">Add to cart</button>
-                                <div class="add"> <span class="product_fav"><i class="fa fa-heart-o"></i></span> <span class="product_fav"><i class="fa fa-opencart"></i></span> </div>
+                        <div class="col-md-4">
+                            <div class="product py-4">
+                                <div class="text-center"> <img src="<?php echo $product['image'] ?>" width="200"> </div>
+                                <div class="about text-center">
+                                    <h5><?php echo $product['name'] ?></h5> <span> &euro; <?= number_format($product['price'], 2) ?></span>
+                                </div>
+                                <div class="cart-button mt-3 px-2 d-flex justify-content-between align-items-center">
+                                    <div class="form-group">
+                                        <button class="btn btn-primary text-uppercase" name="addCart" value="<?php echo $i ?>">Add to cart</button>
+                                        <!--<span class="product_fav"><i class="fa fa-heart-o"></i></span>-->
+                                    </div>
+                                    <div class="col-xs-2">
+                                        <div class="form-group">
+                                            <!--<select  class="custom-select" name = "amount">-->
+                                            <select  class="custom-select" name = "<?php echo 'quantity'.$i ?>">
+                                                <?php for($i = 1; $i <= 10; $i++) {?>
+                                                    <option value="<?php echo $i ?>"><?php echo $i ?></option>
+                                                <?php }?>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-2">
+                                        <div class="form-group">
+                                            <span class="product_fav"><i class="fa fa-opencart"></i></span>
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
-                    </div>
                     <?php endforeach; ?>
                 </div>
             </div>
-
         </fieldset>
 
 
--- index.php
+++ index.php
@@ -6,8 +6,66 @@
 // This line makes PHP behave in a more strict way
 declare(strict_types=1);
 
+
 // We are going to use session variables so we need to enable sessions
 session_start();
+
+// TODO: provide some products (you may overwrite the example)
+$products = [
+    ['name' => 'Cupcake', 'price' => 2.5, 'image' => 'https://i.imgur.com/A8qWeg8.jpeg'],
+    ['name' => 'Blueberry lemon cake', 'price' => 22.5, 'image' => 'https://i.imgur.com/UfQS5r3.jpg'],
+    ['name' => 'Pistachio cardamom cake', 'price' => 22.5, 'image' => 'https://i.imgur.com/cKMzbDx.jpeg'],
+    ['name' => 'Salted pistachio', 'price' => 2.5, 'image' => 'https://i.imgur.com/Qmzp5JV.jpeg'],
+    ['name' => 'Blueberry lemon cake', 'price' => 22.5, 'image' => 'https://i.imgur.com/UfQS5r3.jpg'],
+    ['name' => 'Pistachio cardamom cake', 'price' => 22.5, 'image' => 'https://i.imgur.com/cKMzbDx.jpeg'],
+    ['name' => 'Cupcake', 'price' => 2.5, 'image' => 'https://i.imgur.com/A8qWeg8.jpeg'],
+    ['name' => 'Blueberry lemon cake', 'price' => 22.5, 'image' => 'https://i.imgur.com/UfQS5r3.jpg'],
+    ['name' => 'Pistachio cardamom cake', 'price' => 22.5, 'image' => 'https://i.imgur.com/cKMzbDx.jpeg'],
+];
+
+//var_dump($products);
+
+$cart = [];
+$totalValue = 0;
+$itemCart = [];
+
+//Load up session
+if ( !isset($_SESSION['total']) ) {
+    $_SESSION['total'] = 0;
+}
+if (!isset($_SESSION['amount'])){
+    $_SESSION['amount'] = 0;
+}
+
+/*if(isset($_POST['addCart'])) {
+    $index = $_POST['addCart'];
+    $selectedProduct = $products[$index];
+    //$cart[count($cart)][] = $products[4];
+    addProduct($cart, $selectedProduct);
+
+    //  echo "This is Button1 that is selected " . $currentProduct['name'];
+}*/
+//Add
+if(isset($_POST['addCart'])) {
+    $index = $_POST['addCart'];
+    $quantity = $_POST['quantity'.$index];
+    //$selectedProduct = $products[$index];
+    //$cart[count($cart)][] = $products[4];
+  //  $cart[count($cart)]= [['code' => $index],];
+   // var_dump($cart);
+    addProduct($cart,(int)$index, $products[(int)$index], (int)$quantity);
+    //$itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["name"], 'code'=>$productByCode[0]["code"], 'quantity'=>$_POST["quantity"], 'price'=>$productByCode[0]["price"], 'image'=>$productByCode[0]["image"]));
+
+    //  echo "This is Button1 that is selected " . $currentProduct['name'];
+    foreach (array_column($_SESSION["cart_item"], 'amount') as $item) {
+        $totalValue += $item;
+    }
+    $_SESSION["total"] = $totalValue;
+
+    echo $_SESSION["total"];
+}
+
+
 
 // Use this function when you need to need an overview of these variables
 function whatIsHappening() {
@@ -21,17 +79,83 @@ function whatIsHappening() {
     var_dump($_SESSION);
 }
 
-// TODO: provide some products (you may overwrite the example)
-$products1 = [
-    ['name' => 'Your favourite drink', 'price' => 2.5],
-];
+whatIsHappening();
 
-$products = [
-    ['name' => 'cupcake', 'price' => 2.5, 'image' => 'https://i.imgur.com/A8qWeg8.jpeg'],
-    ['name' => 'cupcake1', 'price' => 2.5, 'image' => 'https://i.imgur.com/A8qWeg8.jpeg'],
-    ['name' => 'cupcake2', 'price' => 2.5, 'image' => 'https://i.imgur.com/A8qWeg8.jpeg'],
-];
+function addProduct($cartArray, $index, $product, $quantity) {
+   // $cartArray[count($cartArray)][] = $product;
 
-$totalValue = 0;
+    if (empty($_SESSION["cart_item"])) {
+       // $cartArray[count($cartArray)][0] = array_merge(['code' => $index, 'amount' => $amount], $product);
+        $amount = $product['price'] * $quantity;
+        $cartArray[count($cartArray)] = array_merge(['code' => $index, 'quantity' => $quantity, 'amount' => $amount], $product);
+        $_SESSION["cart_item"] = $cartArray;
+    }
+    else {
+       /* $codes = array_column($_SESSION["cart_item"], 'code');
+        print_r($codes);*/
+        $codeIndex = array_search($index, array_column($_SESSION["cart_item"], 'code'), true);
+       // echo "codeIndex". $codeIndex."</br>";
+        if (isset($codeIndex)){
+            if (is_numeric($codeIndex)) {
+                $_SESSION["cart_item"][$codeIndex]["quantity"] += $quantity;
+                $addAmount = $_SESSION["cart_item"][$codeIndex]["price"] * $quantity;
+                $_SESSION["cart_item"][$codeIndex]["amount"] += $addAmount;
+            } else {
+                echo "ELSE2 </br>";
+                // $cartArray[count($cartArray)][0] = array_merge(['code' => $index, 'amount' => $amount], $product);
+                $amount = $product['price'] * $quantity;
+                $cartArray[count($cartArray)] = array_merge(['code' => $index, 'quantity' => $quantity, 'amount' => $amount], $product);
+                $_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$cartArray);
+            }
+        }
+       else {
+           echo "ELSE2 </br>";
+           // $cartArray[count($cartArray)][0] = array_merge(['code' => $index, 'amount' => $amount], $product);
+           $amount = $product['price'] * $quantity;
+           $cartArray[count($cartArray)] = array_merge(['code' => $index, 'quantity' => $quantity, 'amount' => $amount], $product);
+           $_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$cartArray);
+
+       }
+    }
+    echo "ADD - cartArray </br>";
+    var_dump($cartArray);
+    echo "</br>ADD - SESSION CART </br>";
+    var_dump($_SESSION["cart_item"]);
+}
+
+    //echo implode('-', $cartArray[0][0]);
+/*    foreach ($cartArray as $i => $item) {
+        echo $i;
+        echo implode(', ', $item[0]);
+        echo $item[0]['name'];
+    }*/
+
+   // echo implode(', ', array_map('getName', $cartArray));
+
+
+
+
+//}
+
+function getName($array)
+{
+    return $array[0]['name'];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //require 'form-view.php';
